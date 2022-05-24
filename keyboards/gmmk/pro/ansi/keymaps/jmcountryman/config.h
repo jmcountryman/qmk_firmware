06d35a3469f521f17eba5f1d4f319084d7c550b2
Jamie Countryman <jamie.countryman@gmail.com>
2022-05-24 18:57:17 -0500

    Add keymap

---
+++ keyboards/gmmk/pro/ansi/keymaps/jmcountryman/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "config_common.h"
+
+/* Turn off LEDs when the computer is asleep */
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
---
+++ keyboards/gmmk/pro/ansi/keymaps/jmcountryman/rules.mk
@@ -0,0 +1,2 @@
+DEBOUNCE_TYPE = asym_eager_defer_pk
+VIA_ENABLE = yes
